d35166603341faa31a14336ba76945c6d00e16fa
Cosen95 <1263215592@qq.com>
2020-11-19 15:55:42 +0800

    :sparkles: Container组件

---
+++ src/components/Container/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="screen-container" :ref="ref">
+    <div v-if="ready">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import autoResize from "./autoResize";
+export default {
+  name: "ScreenContainer",
+  mixins: [autoResize],
+  props: {
+    options: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      ref: "screen-container",
+      allWidth: 0,
+      allHeight: 0,
+      scale: 0,
+      ready: false,
+    };
+  },
+  methods: {
+    afterAutoResizeMixinInit() {
+      this.initConfig();
+      this.setAppScale();
+      this.ready = true;
+    },
+    initConfig() {
+      this.allWidth = this.width || this.originalWidth;
+      this.allHeight = this.height || this.originalHeight;
+      if (this.width && this.height) {
+        this.dom.style.width = `${this.width}px`;
+        this.dom.style.height = `${this.height}px`;
+      } else {
+        this.dom.style.width = `${this.originalWidth}px`;
+        this.dom.style.height = `${this.originalHeight}px`;
+      }
+    },
+    setAppScale() {
+      const currentWidth = document.body.clientWidth;
+      const currentHeight = document.body.clientHeight;
+      this.dom.style.transform = `scale(${currentWidth /
+        this.allWidth}, ${currentHeight / this.allHeight})`;
+    },
+    onResize() {
+      this.setAppScale();
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#screen-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  transform-origin: left top;
+  z-index: 999;
+}
+</style>
