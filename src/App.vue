6787483391d5718314422929492d92d82ee0546f
Cosen95 <1263215592@qq.com>
2020-11-19 17:03:57 +0800

    :art: 调整代码目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <screen />
   </div>
-  <router-view />
 </template>
+<script>
+import Screen from "./views/Screen";
+export default {
+  components: {
+    Screen,
+  },
+};
+</script>
 
-<style>
+<style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  width: 100%;
+  height: 100%;
+  font-family: "Microsoft Yahei", Arial, sans-serif;
+  overflow: hidden;
 }
 </style>
---
+++ src/views/Screen/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <loading v-if="!ready">loading...</loading>
+    <container :options="{ width: 3840, height: 2160 }" v-else></container>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import Loading from "../../components/Loading";
+import Container from "../../components/Container";
+export default {
+  name: "Screen",
+  components: {
+    Loading,
+    Container,
+  },
+  setup() {
+    // const context = getCurrentInstance().ctx;
+    const ready = ref(true);
+    return {
+      ready,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  // background-color: rgb(50, 47, 48);
+  background: rgb(29, 29, 29);
+  color: #fff;
+  font-size: 48px;
+}
+</style>
